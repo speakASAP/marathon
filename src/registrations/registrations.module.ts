c8bc129b0799d82a501c209957f2658284f46bfe
Sergey SpeakASAP <stashok@speakasap.com>
2026-01-27 08:01:44 +0100

    Add NotificationsService to RegistrationsModule providers

--- src/registrations/registrations.module.ts
+++ src/registrations/registrations.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RegistrationsController } from './registrations.controller';
 import { RegistrationsService } from './registrations.service';
 import { PrismaService } from '../shared/prisma.service';
+import { NotificationsService } from '../shared/notifications.service';
 
 @Module({
   controllers: [RegistrationsController],
-  providers: [RegistrationsService, PrismaService],
+  providers: [RegistrationsService, PrismaService, NotificationsService],
 })
 export class RegistrationsModule {}
